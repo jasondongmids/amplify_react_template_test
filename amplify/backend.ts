1840a3e4de48200bd850f8e8332c9a894020a527
JDong <djliang@berkeley.edu>
2025-02-16 20:18:32 -0800

    add externaldb and query

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { aws_dynamodb } from "aws-cdk-lib"; 
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
 });
+
+const extDataSourcesStack = backend.createStack("ExternalDataSources");
+
+const extTable = aws_dynamodb.Table.fromTableName(
+  extDataSourcesStack,
+  "UserStatsTest",
+  "UserStateHxTable"
+)
+
+backend.data.addDynamoDbDataSource(
+  "UserStateHxTable",
+  extTable
+)
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -36,7 +36,20 @@ const schema = a.schema({
       prev_is_correct: a.integer(),
       elapsed_time_total: a.integer(),
       timestamp_created: a.datetime()
-    })
+    }),
+
+    getUserState: a
+      .query()
+      .arguments({id: a.string().required()})
+      .returns(a.ref("Post"))
+      .authorization((allow) => [allow.all()])
+      .handler(
+        a.handler.custom({
+          dataSource: "SourceUserStatsTest",
+          entry: "./getUserState.js"
+        })
+      )
+
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -51,32 +64,3 @@ export const data = defineData({
     },
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
